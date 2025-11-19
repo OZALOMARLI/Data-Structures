b41ea178890083cc80b37b3c46c6a14f1947369c
prefetch4do! <130607739+OZALOMARLI@users.noreply.github.com>
2025-11-19 23:02:58 +0300

    Add files via upload

---
+++ 5'th/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SinglyLinkedList list = new SinglyLinkedList();
+		
+		list.addByIndex(0, 19); 
+
+		list.addByIndex(0, 10); 
+
+		list.addByIndex(1, 49); 
+
+		list.addByIndex(3, 22); 
+
+		list.addByIndex(4, 29);
+	
+	}
+
+}
