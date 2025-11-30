b2021920c2b39866bd820f27c91f01aee9998f0b
prefetch4do! <130607739+OZALOMARLI@users.noreply.github.com>
2025-11-30 04:19:21 +0300

    Update Application.java

--- Stack/src/Application.java
+++ Stack/src/Application.java
@@ -1,16 +1,19 @@
 public class Application {
 
 	public static void main(String[] args) {
+		// Create a new Stack object with a capacity of 5.
 		Stack stack = new Stack(5);
 
+		// Push 5 elements onto the stack.
 		stack.push(4);
 		stack.push(2);
 		stack.push(5);
 		stack.push(9);
 		stack.push(10);
-		System.out.println("Removed Element is : " + stack.pop());
-		System.out.println("Removed Element is : " + stack.pop());
 
+		// Pop two elements from the stack and print them.
+		System.out.println("Removed Element is : " + stack.pop());
+		System.out.println("Removed Element is : " + stack.pop());
 	}
 
 }
